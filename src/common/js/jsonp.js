7866e86d1f35e66f2f92aa6722cd9a77791e7fb3
a712718 <1023940468@qq.com>
2017-11-03 17:52:54 +0800

    promise jsonp

---
+++ src/common/js/jsonp.js
@@ -0,0 +1,24 @@
+import originJsonp from 'jsonp';
+
+export default function jsonp (url, data, options) {
+  url += (url.indexOf('?') < 0 ? '?' : '&') + param(data);
+  let promise = new Promise((resolve, reject) => {
+    originJsonp(url, options, (err, data) => {
+      if (!err) {
+        resolve(data)
+      } else {
+        reject(err)
+      }
+    })
+  })
+  return promise;
+}
+
+export function param (data) {
+  let url = '';
+  for (var k in data) {
+    let value = (data[k] === undefined) ? '' : data[k];
+    url += '&' + k + '=' + encodeURIComponent(value)
+  }
+  return url ? url.substring(1) : ''
+}
