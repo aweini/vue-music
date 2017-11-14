85011ceeb7475106821f711e5f7ee0e13d309ad0
a712718 <1023940468@qq.com>
2017-11-14 18:12:35 +0800

    slider

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -10,8 +10,8 @@
   </div>
 </template>
 <script>
-  // import {addClass} from '@common/js/dom'
-  // import BScroll from 'better-scroll'
+  import {addClass} from '@common/js/dom'
+  import BScroll from 'better-scroll'
   export default {
     name: 'slider',
     props: {
@@ -20,7 +20,58 @@
         default: true
       },
       autoPlay: {
-          
+        type: Boolean,
+        default: true
+      },
+      interval: {
+        type: Number,
+        default: 4000
+      }
+    },
+    data () {
+      return {
+
+      }
+    },
+    mounted () {
+      setTimeout(() => {
+        this._setSliderWidth();
+        this._initSlider();
+      }, 20)
+    },
+    activated () {
+
+    },
+    deactivated () {
+
+    },
+    beforeDestroy () {
+
+    },
+    methods: {
+      _setSliderWidth () {
+        this.children = this.$refs.sliderGroup.children;
+        let width = 0;
+        let sliderWidth = this.$refs.slider.clientWidth;
+        for (let i = 0; i < this.children.length; i++) {
+          let child = this.children[i];
+          addClass(child, 'slider-item');
+          child.style.width = sliderWidth + 'px';
+          width += sliderWidth;
+        }
+        this.$refs.sliderGroup.style.width = width + 'px';
+      },
+      _initSlider () {
+        this.slider = new BScroll(this.$refs.slider, {
+          scrollX: true,
+          scrollY: false,
+          momentum: false,
+          snap: {
+            loop: this.loop,
+            threshold: 0.3,
+            speed: 400
+          }
+        })
       }
     }
   }
