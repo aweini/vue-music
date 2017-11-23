1d28e8e9c29f16f1ea11064da01f248c71f2534b
a712718 <1023940468@qq.com>
2017-11-23 17:53:08 +0800

    recommed

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -9,7 +9,19 @@
         </div>
       </slider>
     </div>
-    
+    <div class="recommend-list">
+      <h1 class="list-title">热门歌单推荐</h1>
+      <ul>
+        <li v-for="item in discList" class="item">
+          <img :src="item.imgurl" class="icon" />
+          <div class="text">
+            <h2 class="name">{{item.creator.name}}</h2>
+            <p class="desc">{{item.dissname}}</p>
+          </div>
+        </li>
+      </ul>
+      
+    </div>
   </div>
 </template>
 <script>
@@ -52,9 +64,46 @@
   }
 </script>
 <style lang="scss">
+@import "src/common/style/variable";
+
 .slider-wrapper{
   position: relative;
   width: 100%;
-   
 }
+.recommend-list{
+  width: 100%;
+  .list-title{
+    height: 65px;
+    line-height: 65px;
+    color: $color-theme;
+    text-align: center;
+    font-size: $font-size-medium;
+  }
+  .item{
+    display: flex;
+    align-items: center;
+    padding: 0 20px 20px 20px;
+    .icon{
+      flex: 0 0 60px;
+      padding-right: 20px;
+      width: 60px;
+    }
+    .text{
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      font-size: $font-size-medium;
+      line-height: 20px;
+      .name{
+        margin-bottom: 10px;
+        color: $color-text;
+      }
+      .desc{
+        color: $color-text-d;
+      }
+    }
+  }
+}
+
 </style>
