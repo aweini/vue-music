8284a94c8ab4a796b8d34ac45f83a348a5ee82cb
a712718 <1023940468@qq.com>
2017-12-01 18:04:03 +0800

    singer slider

---
+++ src/base/listview/listview.vue
@@ -0,0 +1,63 @@
+<template>
+  <scroll class="listview">
+    <ul>
+      <li v-for="group in data" class="list-group">
+        <h2 class="list-group-title">{{group.title}}</h2>
+        <ul>
+          <li v-for="item in group.items" class="list-group-item">
+            <img v-lazy="item.avatar" class="avatar"/>
+            <span class="name">{{item.name}}</span>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </scroll>
+</template>
+<script>
+  import scroll from '../scroll/scroll'
+  export default{
+    props: {
+      data: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+      scroll
+    }
+  }
+</script>
+<style lang='scss'>
+@import 'src/common/style/variable';
+.listview{
+  position: relative;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  .list-group{
+      width: 100%;
+      color: $color-text-ll;
+      font-size: $font-size-medium-x;
+      .list-group-title{
+        height: 40px;
+        line-height:40px;
+        padding-left: 20px;
+        background: $color-highlight-background;
+      }
+      .list-group-item{
+          display: flex;
+          height: 60px;
+          align-items: center;
+          .avatar{
+              width: 40px;
+              height: 40px;
+              border-radius: 40px;
+              margin: 0 40px;
+          }
+          .name{
+
+          }
+      }
+  }
+}
+</style>
--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,10 +1,21 @@
 <template>
-  <div>singer</div>
+  <div class="singer">
+    <listview :data="singers"></listview>
+  </div>
 </template>
 <script>
   import { getSingers } from '@api/singer';
   import { ERR_OK } from '@api/config';
+  import listview from '@base/listview/listview';
+  import Singer from '@common/js/singer';
+
+  const HOT_NAME = '热门';
+  const HOT_SINGER_LEN = 10;
+
   export default{
+    components: {
+      listview
+    },
     props: {
 
     },
@@ -21,12 +32,68 @@
         getSingers().then((res) => {
           if (ERR_OK === res.code) {
             this.singers = res.data.list;
+            this.singers = this._normalizeSinger(this.singers)
             console.log(this.singers);
           }
         })
+      },
+      _normalizeSinger (list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              name: item.Fsinger_name,
+              id: item.Fsinger_mid
+            }))
+          }
+          if (!map[item.Findex]) {
+            map[item.Findex] = {
+              title: item.Findex,
+              items: []
+            }
+          }
+          map[item.Findex].items.push(new Singer({
+            name: item.Fsinger_name,
+            id: item.Fsinger_mid
+          }))
+        })
+
+        console.log('map.......')
+        console.log(map);
+
+        let hot = [];
+        let ret = [];
+        for (let key in map) {
+          if (key === 'hot') {
+            hot.push(map[key])
+          } else if (key.match(/[a-zA-Z]/)) {
+            ret.push(map[key])
+          }
+        }
+
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+
+        let normalizeList = hot.concat(ret);
+        console.log('normalizeList........')
+        console.log(normalizeList);
+        return normalizeList;
       }
     }
   }
 </script>
 <style lang="scss">
+.singer{
+  position: fixed;
+  top: 88px;
+  bottom: 0px;
+  width: 100%;
+}
 </style>
