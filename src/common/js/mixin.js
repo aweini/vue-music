cc786f4fe33a0f364964cffef0e8f43f3627bf0c
a712718 <1023940468@qq.com>
2018-01-18 18:11:38 +0800

    randomPlay

--- src/common/js/mixin.js
+++ src/common/js/mixin.js
@@ -1,4 +1,6 @@
 import {mapGetters, mapMutations} from 'vuex';
+import {shuffle} from '@common/js/util';
+import {playMode} from '@common/js/config';
 export const playerMixin = {
   computed: {
     iconMode () {
@@ -7,16 +9,42 @@ export const playerMixin = {
       return icon;
     },
     ...mapGetters([
-      'mode'
+      'mode',
+      'playList',
+      'currentIndex',
+      'sequenceList',
+      'currentSong'
     ])
   },
   methods: {
     changeMode () {
       let mode = (this.mode + 1) % 3;
       this.setMode(mode);
+      let list = null;
+      if (mode === playMode.random) {
+        list = shuffle(this.sequenceList);
+      } else {
+        list = this.sequenceList;
+      }
+      this.setPlayList(list);
+      this.resetCurrentIndex(list);
+    },
+    resetCurrentIndex (list) {
+      // list.forEach((item, index) => {
+      //   if (item.id === this.currentSong.id) {
+      //     this.setCurrentIndex(index);
+      //   }
+      // })
+      let index = list.findIndex((item) => {
+        return item.id === this.currentSong.id
+      })
+      this.setCurrentIndex(index);
     },
     ...mapMutations({
-      setMode: 'SET_MODE'
+      setMode: 'SET_MODE',
+      setPlayList: 'SET_PLAYLIST',
+      setCurrentIndex: 'SET_CURRENT_INDEX',
+      setPlayingState: 'SET_PLAYING_STATE'
     })
   }
 }
