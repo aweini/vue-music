fc7768261707e86e20dfb84364066ac5d2d54e02
mahong <1023940468@qq.com>
2018-03-22 21:31:14 +0800

    lazy load

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,13 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Recommend from '@components/recommend/recommend';
-import Singer from '@components/singer/singer';
-import Rank from '@components/rank/rank';
-import Search from '@components/search/search';
-import SingerDetail from '@components/singer-detail/singer-detail';
-import Test from '@components/test/test';
-import DiscDetail from '@components/disc-detail/disc-detail';
-import TopList from '@components/top-list/top-list';
+const Singer = () => import(/* webpackChunkName: "singer" */ '@components/singer/singer');
+const Rank = () => import(/* webpackChunkName: "rank" */ '@components/rank/rank');
+const Search = () => import(/* webpackChunkName: "search" */ '@components/search/search');
+const SingerDetail = () => import(/* webpackChunkName: "singerDetail" */ '@components/singer-detail/singer-detail');
+const Test = () => import(/* webpackChunkName: "test" */ '@components/test/test');
+const DiscDetail = () => import(/* webpackChunkName: "discDetail" */ '@components/disc-detail/disc-detail');
+const TopList = () => import(/* webpackChunkName: "topList" */ '@components/top-list/top-list');
+const Recommend = () => import(/* webpackChunkName: "recommend" */ '@components/recommend/recommend');
 
 Vue.use(Router);
 
